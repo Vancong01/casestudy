373a0a74594c23090ea5008f291c82b16d120d2f
Acnologia1 <46494938+Acnologia1@users.noreply.github.com>
2019-12-06 15:01:14 +0700

    casestudy1
    cong1

--- Module2-casestudy-product/src/main/java/com/codegym/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.repository;
-
-import com.codegym.model.Order;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface OrderRepository extends PagingAndSortingRepository<Order,Long> {
-}
